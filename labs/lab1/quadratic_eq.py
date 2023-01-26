310a54fce85e2bffaaacae296dd4d5a4e4400901
Roman Nigmatullin <rmnigmatullin>
2023-01-27 01:11:54 +0300

    added coding and exec for unix

--- labs/lab1/quadratic_eq.py
+++ labs/lab1/quadratic_eq.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import numpy as np
 
 coefficients = np.array([1, -30.9, 238.7])
--- labs/lab1/series.py
+++ labs/lab1/series.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding: utf-8
+
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
