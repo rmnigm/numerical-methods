db7a5cfdeacdf4a42f2ab9ed8e9c77609bdfef49
Roman Nigmatullin <rmnigmatullin>
2023-01-27 01:14:24 +0300

    added fixed bit depth to utils

---
+++ utils/python/vm_models.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+from math import ceil
+
+
+def fixed_bit_depth(x, m):
+    p = 0
+    while x >= 1:
+        x /= 10
+        p += 1
+    x *= 10**p
+    return ceil(x * 10**(m - p)) / 10**(m - p)
