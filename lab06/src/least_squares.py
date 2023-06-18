8198de29bd014454be44488aa6eccf75242d5773
Roman Nigmatullin <rmnigmatullin>
2023-06-18 16:09:51 +0300

    fixed lstsq task

--- lab06/src/least_squares.py
+++ lab06/src/least_squares.py
@@ -20,21 +20,23 @@ def approximate(x, y, m):
         weight = lstsq(x, y, m)
         sigma[m] = np.sqrt((1 / (n - m)) * ((y - X.dot(weight)) ** 2).sum())
         weights[m] = weight
-    plt.plot(sigma.keys(), sigma.values())
+    plt.bar(sigma.keys(), sigma.values())
     plt.xlabel('m')
     plt.savefig('plots/histogram_lstsq.png', dpi=400)
     plt.close()
     print('Enter optimal M = ', end='')
     optimal = int(input())
-    return weights[optimal], optimal
+    return weights, optimal
 
 
-def plot(weights, m, x, y):
+def plot(weights, ms, x, y):
     xs = np.arange(min(x) - 0.1, max(x) + 0.1, 0.01)
-    X = np.stack([xs ** k for k in range(m)]).T
-    Y = X.dot(weights)
     plt.scatter(x, y, label='data points', color='black')
-    plt.plot(xs, Y, label='polynomial interpolation')
+    for m in ms:
+        w = weights[m]
+        X = np.stack([xs ** k for k in range(m)]).T
+        Y = X.dot(w)
+        plt.plot(xs, Y, label=f'$P_{ {m} }$')
     plt.legend()
     plt.savefig('plots/least_squares.png', dpi=400)
     plt.close()
@@ -44,5 +46,5 @@ x = np.array([-3.2, -2.66, -2.12, -1.58, -1.04, -0.5, 0.04, 0.58, 1.12, 1.66, 2.
 y = np.array([-0.173, -0.574, -1.811, -1.849, 0.123, 1.462, 2.399, 1.3, 1.703, -2.045, 2.817])
 n = len(x)
 
-optimal_weights, optimal_m = approximate(x, y, 11)
-plot(optimal_weights, optimal_m, x, y)
+weights, optimal_m = approximate(x, y, 11)
+plot(weights, range(6, 11), x, y)
