dd7ee8ec25ecea3cee1ef3b86cae420b4f543db9
Roman Nigmatullin <rmnigmatullin>
2023-06-18 15:52:11 +0300

    fixed splines to quadratic

--- lab06/src/spline.py
+++ lab06/src/spline.py
@@ -3,22 +3,31 @@ import scipy.interpolate as intp
 import matplotlib.pyplot as plt
 
 left, right = 1, 1.28
-fig = plt.figure()
-ks = [2, 3, 4, 5]  # 6, 8, 10, 12
-for i in range(4):
+fig1, ax1 = plt.subplots(nrows=1, ncols=1)
+fig2, ax2 = plt.subplots(nrows=3, ncols=2, figsize=(9, 9))
+ks = [3, 4, 6, 8, 10, 12]
+for i in range(6):
     k = ks[i]
-    x = np.arange(left, right, (right - left) / k)
-    wide_x = np.arange(left, right, (right - left) / (3 * k))
+    x = np.linspace(left, right, k, endpoint=True)
+    wide_x = np.linspace(left, right, 3 * k, endpoint=True)
     y = 12 * np.sin(np.exp(x))
     wide_y = 12 * np.sin(np.exp(wide_x))
     
-    poly = intp.CubicSpline(x, y)
+    poly = intp.interp1d(x, y, kind='quadratic')
     
-    plt.plot(wide_x, np.abs(wide_y - poly(wide_x)), label=f'{k}')
+    ax1.plot(wide_x, np.abs(wide_y - poly(wide_x)), label=f'{k}')
+    ax2[i // 2][i % 2].plot(wide_x, wide_y, label='f(x)')
+    ax2[i // 2][i % 2].plot(wide_x, poly(wide_x), label=f'{k} knots')
+    ax2[i // 2][i % 2].legend()
     
 
-plt.legend()
-plt.suptitle('Error of splines by knot quantity')
-plt.show()
-plt.savefig('plots/spline_by_knot_error_1.png', dpi=400)
-plt.close()
+fig1.legend()
+fig1.suptitle('Error of splines by knot quantity')
+fig1.savefig('plots/spline_by_knot_error.png', dpi=400)
+plt.close(fig1)
+
+
+fig2.suptitle('Splines by knot quantity')
+fig2.tight_layout()
+fig2.savefig('plots/spline_by_knot.png', dpi=400)
+plt.close(fig2)
