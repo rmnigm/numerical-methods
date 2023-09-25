356951588186f85798411c3364008da8de72ae14
Roman Nigmatullin <rmnigmatullin>
2023-09-25 17:36:38 +0300

    new ode functions in microlib

--- tools/utils/utils/ode.py
+++ tools/utils/utils/ode.py
@@ -24,16 +24,6 @@ def extrapolate_adams(f, y0, t0, t_end, h):
     return y
 
 
-@njit
-def eyler(f, y0, t0, t_end, h):
-    n = int((t_end - t0) / h)
-    y = np.empty(n + 1)
-    y[0] = y0
-    for i in range(n):
-        y[i + 1] = (y[i] + h * f(t0 + i * h, y[i]))
-    return y
-
-
 def rk4_step(f, t, s, h):
     ss = 4
     k = [f(t, s)]
@@ -62,12 +52,62 @@ def rk4_nsteps(f, y0, t0, t_end, h):
     return arr[:, 1]
 
 
-# def rk2_adaptive_step()
-
-
 def runge_error(solver, f, y0, t0, t_end, h, p):
     normal_precision = solver(f, y0, t0, t_end, h)
     double_precision = solver(f, y0, t0, t_end, h / 2)
     errors = (double_precision[::2] - normal_precision) / (2 ** p - 1)
     return errors
-    
+
+
+def runge_error_step(solver_step, f, y0, t0, h, p):
+    y_h = solver_step(f, y0, t0, h)
+    y_1_h2 = solver_step(f, y0, t0, h / 2)
+    y_2_h2 = solver_step(f, y_1_h2, t0 + h / 2, h / 2)
+    error = (y_h - y_2_h2) / (2 ** p - 1)
+    return error
+
+
+def eyler_step(f, y0, t0, h):
+    return y0 + h * f(t0, y0)
+
+
+def eyler(f, y0, t0, t_end, h):
+    n = int((t_end - t0) / h)
+    y = np.empty(n + 1)
+    y[0] = y0
+    for i in range(n):
+        y[i + 1] = (y[i] + h * f(t0 + i * h, y[i]))
+    return y
+
+
+def eyler_modified_step(f, y0, t0, h):
+    y_pred = y0 + h * f(t0, y0)
+    y_corr = y0 + h * (f(t0, y0) + f(t0 + h, y_pred)) / 2
+    return y_corr
+
+
+def eyler_modified(f, y0, t0, t_end, h):
+    n = int((t_end - t0) / h)
+    y = np.empty(n + 1)
+    y[0] = y0
+    for i in range(n):
+        y[i + 1] = eyler_modified_step(f, y[i], t0 + i * h, h)
+    return y
+
+
+def eyler_adaptive(f, y0, t0, t_end, h0, tol):
+    ys = [y0]
+    ts = [t0]
+    y_prev = y0
+    t, h = t0, h0
+    while t < t_end:
+        y = eyler_modified_step(f, y_prev, t, h)
+        err = abs(runge_error_step(eyler_modified_step, f, y_prev, t, h, 2))
+        factor = min(max(tol / np.sqrt(2 * err), 0.3), 2)
+        if factor >= 1:
+            ys.append(y)
+            y_prev = y
+            t += h
+            ts.append(t)
+        h = 0.9 * h * factor
+    return ys, ts
